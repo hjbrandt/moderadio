580813791801bea9c7811c2a25ef8e24c9368af2
Sveven <even.romo@gmail.com>
2019-09-05 20:34:02 +0200

    Removed all boilerplate in Sanity Studio as well as in Next frontend

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,21 +1,12 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
 
-class IndexPage extends React.Component {
-  static propTypes = {
-    config: PropTypes.object
-  }
+const index = () => {
+  return (
+    <Layout>
+      <div>LOL</div>
+    </Layout>
+  );
+};
 
-  render () {
-    const {config} = this.props
-    return (
-      <Layout config={config}>
-        <h1>No route set</h1>
-        <h2>Setup automatic routes in sanity or custom routes in next.config.js</h2>
-      </Layout>
-    )
-  }
-}
-
-export default IndexPage
+export default index;
